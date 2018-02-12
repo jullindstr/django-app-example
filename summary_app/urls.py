b0f8f2c61f616bf15ec8dfeb88ab80af2a39ef4f
Julia Lindström <julia.lindstrom@student.uib.no>
2018-02-12 12:42:16 +0000

    Added index page with links to invidual tables

--- summary_app/urls.py
+++ summary_app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, include
 from . import views
 urlpatterns = [
  
-    url(r'^$', views.dataset_table, name = 'dataset_table'),
-    
+    url(r'^$', views.index, name = 'index'),
+	url(r'^(?P<table_id>[0-9]+)/$', views.dataset_table, name = 'dataset_table'),
     
 ] 
--- summary_app/views.py
+++ summary_app/views.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
 from geospaas.catalog.models import Dataset
 
 # Create your views here.
 
-def dataset_table(request):
+def dataset_table(request, table_id):
+    dataset = Dataset.objects.get(pk = table_id)
+    context = {'dataset': dataset}
+    return render(request,'summary_app/dataset_table.html',context)
+
+def detail(request, table_id):
+     return HttpResponse("You are looking at table %s.", table_id)
+
+
+def index(request):
     datasets = Dataset.objects.all()
     context = {'datasets': datasets}
-    return render(request,'summary_app/dataset_table.html',context = context)
-
-
+    return render (request, 'summary_app/index.html', context)
