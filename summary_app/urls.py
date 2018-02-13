b2be5d64d6bde33e39cffe046a7d4d7c3f42164f
Julia Lindström <julia.lindstrom@student.uib.no>
2018-02-13 11:13:13 +0000

    Changed to generic views.

--- summary_app/urls.py
+++ summary_app/urls.py
@@ -1,10 +1,11 @@
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 from . import views
+
 urlpatterns = [
  
-    url(r'^$', views.index, name = 'index'),
-	url(r'^(?P<table_id>[0-9]+)/$', views.dataset_table, name = 'dataset_table'),
+    url(r'^$',views.IndexView.as_view(), name = 'index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DatasetView.as_view(), name = 'dataset_table'),
     
 ] 
--- summary_app/views.py
+++ summary_app/views.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+
 from geospaas.catalog.models import Dataset
 
-# Create your views here.
+class DatasetView(generic.DetailView):
+    model = Dataset
+    template_name = 'summary_app/dataset_table.html'
+	
 
-def dataset_table(request, table_id):
-    dataset = Dataset.objects.get(pk = table_id)
-    context = {'dataset': dataset}
-    return render(request,'summary_app/dataset_table.html',context)
-
-def detail(request, table_id):
-     return HttpResponse("You are looking at table %s.", table_id)
-
-
-def index(request):
-    datasets = Dataset.objects.all()
-    context = {'datasets': datasets}
-    return render (request, 'summary_app/index.html', context)
+class IndexView(generic.ListView): 	 
+    context_object_name = 'datasets'
+    template_name = 'summary_app/index.html'
+    
+    def get_queryset(self):
+        return Dataset.objects.all()
+	 
