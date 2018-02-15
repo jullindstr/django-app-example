439b3d3fc39b920bbc10915aaa67601ce18d8079
Julia Lindström <julia.lindstrom@student.uib.no>
2018-02-15 10:21:23 +0000

    Added json

--- summary_app/urls.py
+++ summary_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
  
     url(r'^$',views.IndexView.as_view(), name = 'index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DatasetView.as_view(), name = 'dataset_table'),
+	url(r'^result/', views.output_json, name = 'result'),
+    url(r'^(?P<pk>[0-9]+)/json/$',views.output_json, name = 'output_json'),
+	#url(r'^json/$',views.output_json, name = 'output_json'),
     
 ] 
